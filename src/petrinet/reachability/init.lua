2b0659a67ff787f7c2626a017a799de3cbbebd0b
Alban Linard <alban@linard.fr>
2018-03-16 11:16:01 +0100

    Add reachability graph.

---
+++ src/petrinet/reachability/init.lua
@@ -0,0 +1,100 @@
+local State = require "petrinet.state"
+
+local Mt    = {}
+local Graph = setmetatable ({}, Mt)
+
+Graph.__index = Graph
+
+function Mt.__call (_, t)
+  return setmetatable ({
+    petrinet  = t.petrinet,
+    free      = t.free,
+    traversal = t.traversal or Graph.depth_first,
+  }, Graph)
+end
+
+function Graph.depth_first (work)
+  local result = work [#work]
+  work [#work] = nil
+  return result
+end
+
+function Graph.breadth_first (work)
+  local result = work [1]
+  table.remove (work, 1)
+  return result
+end
+
+local States_mt = {}
+local States    = setmetatable ({}, States_mt)
+
+function States_mt.__call (_, petrinet)
+  local places = {}
+  for _, place in petrinet:places () do
+    places [#places+1] = place
+  end
+  return setmetatable ({
+    petrinet = petrinet,
+    places   = places,
+  }, States)
+end
+
+function States.__add (states, state)
+  local current = states
+  -- Free tokens:
+  if not current [state.free] then
+    current [state.free] = {}
+  end
+  current = states [state.free]
+  -- Places:
+  for _, place in ipairs (states.places) do
+    if not current [state.marking [place]] then
+      current [state.marking [place]] = {}
+    end
+    current = current [state.marking [place]]
+  end
+  if current.state then
+    return current.state
+  else
+    current.state = state
+    return nil
+  end
+end
+
+function Graph.__call (graph)
+  local initial = State {
+    petrinet = graph.petrinet,
+    free     = graph.free,
+  }
+  local unique = States (graph.petrinet)
+  local _      = unique + initial
+  local work   = { initial }
+  local states = { initial }
+  while #work ~= 0 do
+    local state = graph.traversal (work)
+    for transition in state:enabled () do
+      local successor = state (transition)
+      local existing  = unique + successor
+      successor = existing or successor
+      -- local existing = false
+      -- for _, s in ipairs (states) do
+      --   if s == successor then
+      --     existing  = true
+      --     successor = s
+      --     break
+      --   end
+      -- end
+      if not existing then
+        states [#states+1] = successor
+        work   [#work  +1] = successor
+      end
+      state.successors [transition] = successor
+    end
+    -- if #states % 100 == 0 then
+    --   print ("#states: ", #states, "#work: ", #work)
+    -- end
+  end
+  return initial, states
+end
+
+return Graph
---
+++ src/petrinet/reachability/init_spec.lua
@@ -0,0 +1,70 @@
+local assert       = require "luassert"
+local Reachability = require "petrinet.reachability"
+local petrinet     = require "petrinet.example"
+
+describe ("Reachability Reachability", function ()
+
+  it ("can be created with an implicit traversal", function ()
+    local reachability = Reachability {
+      petrinet = petrinet,
+      free     = 10,
+    }
+    assert.are.equal (getmetatable (reachability), Reachability)
+    assert.are.equal (reachability.traversal, Reachability.depth_first)
+  end)
+
+  it ("can be created with an explicit traversal", function ()
+    local reachability = Reachability {
+      petrinet  = petrinet,
+      free      = 10,
+      traversal = Reachability.breadth_first,
+    }
+    assert.are.equal (getmetatable (reachability), Reachability)
+    assert.are.equal (reachability.traversal, Reachability.breadth_first)
+  end)
+
+  it ("can compute the reachability graph of a Petri net (depth first)", function ()
+    local reachability = Reachability {
+      petrinet  = petrinet,
+      free      = 2,
+      traversal = Reachability.breadth_first,
+    }
+    local initial, states = reachability (petrinet)
+    assert.are.equal (#states, 5)
+    assert.is_nil     (initial.successors [petrinet.t0])
+    assert.is_nil     (initial.successors [petrinet.t1])
+    assert.is_nil     (initial.successors [petrinet.t2])
+    assert.is_nil     (initial.successors [petrinet.t3])
+    assert.is_not_nil (initial.successors [petrinet.t4])
+    assert.is_not_nil (initial.successors [petrinet.t5])
+    assert.is_not_nil (initial.successors [petrinet.t6])
+  end)
+
+  it ("can compute the reachability graph of a Petri net (breadth first)", function ()
+    local reachability = Reachability {
+      petrinet  = petrinet,
+      free      = 2,
+      traversal = Reachability.breadth_first,
+    }
+    local initial, states = reachability (petrinet)
+    assert.are.equal (#states, 5)
+    assert.is_nil     (initial.successors [petrinet.t0])
+    assert.is_nil     (initial.successors [petrinet.t1])
+    assert.is_nil     (initial.successors [petrinet.t2])
+    assert.is_nil     (initial.successors [petrinet.t3])
+    assert.is_not_nil (initial.successors [petrinet.t4])
+    assert.is_not_nil (initial.successors [petrinet.t5])
+    assert.is_not_nil (initial.successors [petrinet.t6])
+  end)
+
+  it ("can compute the reachability graph of the example", function ()
+    local reachability = Reachability {
+      petrinet  = petrinet,
+      free      = 10,
+      traversal = Reachability.breadth_first,
+    }
+    local _, states = reachability (petrinet)
+    assert.are.equal (#states, 1244)
+  end)
+
+end)
