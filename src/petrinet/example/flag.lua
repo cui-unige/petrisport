7d41e21256dc7ed9064b5b7475b653de811e9ca8
Alban Linard <alban@linard.fr>
2018-05-16 15:08:08 +0200

    Fix marking in the flag example.

--- src/petrinet/example/flag.lua
+++ src/petrinet/example/flag.lua
@@ -3,19 +3,19 @@ local result   = Petrinet ()
 
 result.p0 = result:place {
   name    = "P0",
-  marking = 0,
+  marking = 1,
   x       = 0,
   y       = 2,
 }
 result.p1 = result:place {
   name    = "P1",
-  marking = 0,
+  marking = 1,
   x       = -4,
   y       = 0,
 }
 result.p2 = result:place {
   name    = "P2",
-  marking = 0,
+  marking = 1,
   x       = -3,
   y       = 0,
 }
@@ -33,25 +33,25 @@ result.p4 = result:place {
 }
 result.p5 = result:place {
   name    = "P5",
-  marking = 2,
+  marking = 1,
   x       = 0,
   y       = 0,
 }
 result.p6 = result:place {
   name    = "P6",
-  marking = 2,
+  marking = 0,
   x       = 2,
   y       = 0,
 }
 result.p7 = result:place {
   name    = "P7",
-  marking = 2,
+  marking = 0,
   x       = 4,
   y       = 0,
 }
 result.p8 = result:place {
   name    = "P8",
-  marking = 2,
+  marking = 0,
   x       = 0,
   y       = -2,
 }
